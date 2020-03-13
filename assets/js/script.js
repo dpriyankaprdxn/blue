d8931d47ba370626637807d978cb1f446ecd4bd9
jitendra <jitendrak@prdxn.com>
2020-03-13 14:00:35 +0530

    hamburger js completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,19 @@
-/* Author: 
+var hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click',hamburgerToggle);
 
-*/
+function hamburgerToggle() {
+  document.querySelector('nav').classList.toggle('showhide');
+  document.querySelector('.hamburger').classList.toggle('open');
+}
 
+$(window).on('load', function() {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	$('.review-slider').slick({
+	  dots: true,
+	  arrows: false,
+	  infinite: true,
+	  autoplay: true,
+	  speed: 300,
+	  slidesToShow: 1
+	});
+});
